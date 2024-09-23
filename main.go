8cfac075e23e22518349feec2c634d65901e3d0e
atkzw3 <akiyoshi.t3@gmail.com>
2024-09-23 22:42:10 +0900

    :recycle: add router method

--- main.go
+++ main.go
@@ -8,8 +8,7 @@ import (
 	"gin-market/services"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"log"
-	"os"
+	"gorm.io/gorm"
 )
 
 // https://gin-gonic.com/ja/docs/quickstart/
@@ -19,15 +18,13 @@ import (
 func main() {
 	infra.Initialize()
 	db := infra.SetupDB()
-	log.Println(os.Getenv("ENV"))
-	//
-	//items := []models.Item{
-	//	{ID: 1, Name: "商品1", Price: 1000, Description: "説明1", SoldOut: false},
-	//	{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
-	//	{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
-	//}
-	//ir := repositories.NewItemRepository(items)
 
+	r := setupRouter(db)
+
+	r.Run("localhost:8080") // 0.0.0.0:8080 でサーバーを立てます。
+}
+
+func setupRouter(db *gorm.DB) *gin.Engine {
 	ir := repositories.NewDBItemRepository(db)
 	is := services.NewItemService(ir)
 	ic := controllers.NewItemController(is)
@@ -53,5 +50,5 @@ func main() {
 	authR.POST("/signup", authController.SignUp)
 	authR.POST("/login", authController.Login)
 
-	r.Run("localhost:8080") // 0.0.0.0:8080 でサーバーを立てます。
+	return r
 }
